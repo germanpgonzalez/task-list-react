6d61b7ce56ddc5aca1a584ca16f7eddeae59df2a
Germán Pablo Gonzalez <germancai@hotmail.com>
2024-09-27 13:53:18 -0300

    Renderizado cuando no hay task disponibles

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,9 +3,11 @@ import { TaskCard } from "../components/TaskCard";
 export const TaskList = ({ data }) => {
   return (
     <>
-      {data.map((task) => {
-        return <TaskCard task={task} key={task.id} />;
-      })}
+      {data.length === 0 ? <h1>No hay tareas disponibles</h1> :
+        data.map((task) => {
+          return <TaskCard task={task} key={task.id} />
+        })
+      }
     </>
   );
 };
